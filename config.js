f407a58d91d3db06e9710eec94e055400b6ebefa
moelhalawany38-jpg <mo.elhalawany38@gmail.com>
2026-09-03 12:15:50 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // ============================================================
 // حط هنا بيانات مشروع Supabase بتاعك (من Project Settings -> API)
 // ============================================================
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "sb_publishable_7ppBB5I4ao8SbnuQnnetMQ_w_YA18MV";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2eXlkb25zd2J5aWNzbWdyeXNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjE2NjIsImV4cCI6MjEwMzk5NzY2Mn0.TbMqR0VPAunsP74O6DzJi9iZAvoBHKzjkGTwp_V4CHo";
 
 // اللاحقة اللي بتتضاف لاسم المستخدم عشان تتحول لإيميل داخلي (منين ما تلمسهاش)
 const USERNAME_DOMAIN = "@mdp.local";
