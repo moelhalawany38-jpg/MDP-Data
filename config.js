524f8d226479ee14721973526a1795767522e8a0
moelhalawany38-jpg <mo.elhalawany38@gmail.com>
2026-09-03 13:16:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// ============================================================
-// حط هنا بيانات مشروع Supabase بتاعك (من Project Settings -> API)
-// ============================================================
-const SUPABASE_URL = "sb_publishable_7ppBB5I4ao8SbnuQnnetMQ_w_YA18MV";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2eXlkb25zd2J5aWNzbWdyeXNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjE2NjIsImV4cCI6MjEwMzk5NzY2Mn0.TbMqR0VPAunsP74O6DzJi9iZAvoBHKzjkGTwp_V4CHo";
-// اللاحقة اللي بتتضاف لاسم المستخدم عشان تتحول لإيميل داخلي (منين ما تلمسهاش)
+const SUPABASE_URL = "https://xvyydonswbyicsmgrysi.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_7ppBB5I4ao8SbnuQnnetMQ_w_YA18MV";
+
 const USERNAME_DOMAIN = "@mdp.local";
