155747aece53571ec2839dc2710b2a13d973f2ba
moelhalawany38-jpg <mo.elhalawany38@gmail.com>
2026-09-03 12:27:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,5 @@
 // ============================================================
 const SUPABASE_URL = "sb_publishable_7ppBB5I4ao8SbnuQnnetMQ_w_YA18MV";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2eXlkb25zd2J5aWNzbWdyeXNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjE2NjIsImV4cCI6MjEwMzk5NzY2Mn0.TbMqR0VPAunsP74O6DzJi9iZAvoBHKzjkGTwp_V4CHo";
-
 // اللاحقة اللي بتتضاف لاسم المستخدم عشان تتحول لإيميل داخلي (منين ما تلمسهاش)
 const USERNAME_DOMAIN = "@mdp.local";
