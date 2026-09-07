3f32d49d89ff7f6d07afa2f90d39600e6cc148df
moelhalawany38-jpg <mo.elhalawany38@gmail.com>
2026-09-07 12:21:46 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://xvyydonswbyicsmgrysi.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_7ppBB5I4ao8SbnuQnnetMQ_w_YA18MV";
+
+const USERNAME_DOMAIN = "@mdp.local";
